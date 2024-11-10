d252715e284695cd028cc3dff5e12c227e041296
André Maltez Nunes <andremalteznunes@sapo.pt>
2024-11-10 17:20:11 +0000

    Added aliases Added and modified some ls aliases; Added an alias for verbose copying; Added some cd aliases.

--- alias.bash
+++ alias.bash
@@ -5,12 +5,26 @@
 # Mingw alias
 alias gccw='x86_64-w64-mingw32-gcc'
 
-# some more ls aliases
-alias ll='ls -alF'
-alias la='ls -A'
+# ls aliases
+alias ll='ls -lF'
+alias la='ls -AlF'
+alias lt='ls -lAtr'     # sort by date
+alias labc='ls -lAp'    # alphabetical sort
 alias l='ls -CF'
+
+# confirm overwrites on mv and cp
+# alias cp='cp -vi'
+# alias mv='mv -vi'
+
+# verbose copying
+alias cpv='rsync -avh --info=progress2'
 
 # Add an "alert" alias for long running commands.  Use like so:
 #   sleep 10; alert
 alias alert='notify-send --urgency=low -i "$([ $? = 0 ] && echo terminal || echo error)" "$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')"'
 
+# cd aliases
+alias ..='cd ..'
+alias cd..='cd ..'
+alias ...='cd ../..'
+alias ....='cd ../../..'
